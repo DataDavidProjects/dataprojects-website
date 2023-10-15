b7ce5e3974c31934e19120185011d2d5c36c8edc
Davide Lupis <davide_lupis@yahoo.it>
2023-10-15 13:18:54 +0200

    Sharing

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export function constructMetadata({
   title = "DataProjects",
-  description = "DataProjects is a community of young professionals to make Data and AI accessible to small business.",
+  description = "DataProjects is a community of young professionals to make Data and AI accessible to small businesses.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
   noIndex = false,
